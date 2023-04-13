a286e2ef4f5f122595d6bc14abce29bfb87c483d
Lee JaeHyeok <dhfif718@gmail.com>
2023-04-13 17:13:41 +0900

    [feat] data.sql 데이터 자동삽입

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into member (name, age) VALUES ('이재혁','29')
+insert into member (name, age) VALUES ('염승호','28')
+insert into member (name, age) VALUES ('정용원','27')
+
+insert into shows (title, content) VALUES ('제목1','내용1')
+insert into shows (title, content) VALUES ('제목2','내용2')
+insert into shows (title, content) VALUES ('제목3','내용3')
