4388e638cdb7e13a32ff1698207cc1939c5e8853
Lee JaeHyeok <dhfif718@gmail.com>
2023-04-14 14:16:02 +0900

    [feat] 매개변수 여러개를 활용

--- src/main/java/com/prac/graphql/domain/controller/MemberController.java
+++ src/main/java/com/prac/graphql/domain/controller/MemberController.java
@@ -5,6 +5,7 @@ import com.prac.graphql.domain.entity.Member;
 import com.prac.graphql.domain.mapper.MemberMapper;
 import com.prac.graphql.domain.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
@@ -17,6 +18,7 @@ import java.util.List;
 * EndPoint : http://localhost:8080/graphql
 * Test 접속 URL : http://localhost:8080/graphiql 에 쿼리를 입력
 * */
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
@@ -50,6 +52,36 @@ public class MemberController {
         List<Member> allMember = memberService.findAllMember();
         return memberMapper.memberListToMemberResponseDtoList(allMember);
     }
+
+
+    /*
+    * 활용, 매개변수를 여러개로 받기
+    * 매개변수에 값을 넣지않고 줄떄는 null 값으로 보내짐
+    * */
+    @QueryMapping
+    public Member getMemberAdvance(@Argument(name = "id") Long id,
+                                   @Argument(name = "search") String search,
+                                   @Argument(name = "category") String category,
+                                   @Argument(name = "address") String address,
+                                   @Argument(name = "filter") String filter,
+                                   @Argument(name = "start") String start,
+                                   @Argument(name = "end") String end,
+                                   @Argument(name = "page") Integer page,
+                                   @Argument(name = "size") Integer size ) {
+
+        log.info("id = {}", id);
+        log.info("search = {}", search);
+        log.info("category = {}", category);
+        log.info("address = {}", address);
+        log.info("filter = {}", filter);
+        log.info("start = {}", start);
+        log.info("end = {}", end);
+        log.info("page = {}", page);
+        log.info("size = {}", size);
+
+        return memberService.findMemberById(Long.valueOf(id));
+    }
+
 
     /*
     * @MutationMapping
